33f57ba2ded1731ef5b885c8988e2dd676104590
kangwook <rlarkddnr1686@naver.com>
2023-01-13 12:07:16 +0900

    스프링 부트 환경설정 및 테스트
    # dependency 설정
    # application.yml 생성
    # h2 DB 테스트

---
+++ src/test/java/jpabook/jpashop2/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional //테스트코드에 있으면 db를 롤백해버림
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertEquals(findMember.getId(),member.getId() );
+        assertEquals(findMember.getUsername(),member.getUsername() );
+        assertEquals(findMember,member);
+
+     }
+
+}
+
+
+
+
+
