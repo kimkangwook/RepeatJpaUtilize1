a0e633e9f2dce0c0d68bacc36be363e3113a2e54
kangwook <rlarkddnr1686@naver.com>
2023-01-14 07:02:23 +0900

    HomeController 작성 및 html,css,js 파일 생성
    # localhost HomeController
    # home view template 작성

--- src/main/java/jpabook/jpashop2/Repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop2/Repository/OrderRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,5 +22,14 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m"
+                + " where o.status = :status " +
+                "and m.name like :name", Order.class)
+                .setParameter("status",orderSearch.getOrderStatus())
+                .setParameter("name",orderSearch.getMemberName())
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+    }
+
 }
