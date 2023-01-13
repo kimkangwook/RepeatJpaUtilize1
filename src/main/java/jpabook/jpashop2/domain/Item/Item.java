42170e1f95e442eda042719f7058bd3f24add807
kangwook <rlarkddnr1686@naver.com>
2023-01-13 19:36:23 +0900

    Item Repository 생성
    # Item addStock, removeStock 생성 - 객체 자체의 비즈니스 로직 생성
    # Item 저장, 조회 기능 구현

--- src/main/java/jpabook/jpashop2/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop2/domain/Item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop2.domain.Item;
 
+import jpabook.jpashop2.Exception.NotEnoughStockException;
 import jpabook.jpashop2.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,4 +26,22 @@ public abstract class Item {
 
     @ManyToMany
     private List<Category> categories = new ArrayList<>();
+
+
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void remove(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock<0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
