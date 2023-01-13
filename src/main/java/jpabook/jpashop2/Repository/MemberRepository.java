08125a82f08dd45541a235bccfda75b3499e9daa
kangwook <rlarkddnr1686@naver.com>
2023-01-13 15:44:57 +0900

    Service 생성
    # MemberService 생성
    # 중복 회원 검증 : 멀티 쓰레드에서 동시 접근을 방어해주기 위해 DB에 Unique 제약조건 걸어주는 게 안전함

--- src/main/java/jpabook/jpashop2/Repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop2/Repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop2.Repository;
 
 import jpabook.jpashop2.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,10 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em; // 스프링 data jpa가 지원해줌 원래는 엔티티매니저는 @Autowired 안됨
 
     public void save(Member member) {
         em.persist(member);
